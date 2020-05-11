ed0b5501294379d0963909e7c0704b86d6662e54
Ekaterina Baibuz <ekaterina.baibuz@gmail.com>
2020-05-11 15:58:44 +0300

    print table with pandas DataFrame

--- mult_table.py
+++ mult_table.py
@@ -1,21 +1,48 @@
+# The program reads in an integer number from command line,
+# creates a multiplication table,
+# and prints it
+# Requires pandas and numpy modules installed
+# by E. baibuz
+
 import sys,os
 sys.path.append(os.getcwd())
 import module
 from module import *
+import numpy as np
+
+#Reading command line arguments
 if "help" in sys.argv:
+    # print help message if asked
     print("The program outputs a multiplication table up to the input number. Usage: ")
-    print("python %s N"%sys.argv[0])
+    print("python3 %s N "%sys.argv[0])
     print("where N is an integer number")
+    print("pandas is 1 or 0, specifies if pandas module is used for printing the table")
 else:
     if len(sys.argv)<2:
+        # check if number provided
         print("Wrong input, see help:")
-        print("python %s help "%sys.argv[0])
-        exit
+        print("python3 %s help "%sys.argv[0])
+        sys.exit(0)
     else:
         N_input = sys.argv[1]
+        #making sure N is integer
         if if_integer(N_input):
             N = int(N_input)
             print("Multiplication table for N = %i"%N)
         else:
             print("Input number N = %s is not an integer"%N_input)
-            
+            sys.exit(0)
+
+# main program starts here
+table = np.zeros((N, N), dtype=int)
+for i in range(N):
+    for j in range(N):
+        # assigning multiplication table
+        table[i,j] = (i+1)*(j+1)
+        #print (' '.join(map(str, table[i,:]))) # for printing is pandas not available. not recomended!
+
+#printing the table with pandas module
+import pandas
+df = pandas.DataFrame(table)
+print(df.to_string(index = False, header=False))
+
