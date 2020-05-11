2cc9c4a3e401f5bcac1000647387972318a2c916
Ekaterina Baibuz <ekaterina.baibuz@gmail.com>
2020-05-11 19:18:15 +0300

    extract functions from main to separate module
    -add check_command_line(arguments)
    -add readin_integer(arguments)
    -add generate_mult_table
    -remove if_integer function

--- module.py
+++ module.py
@@ -1,6 +1,44 @@
-def if_integer(n):
+import numpy as np
+import sys
+def isinteger(n):
     try:
         int(n)
         return True
     except ValueError:
         return False
+
+def check_command_line(arguments):
+    if "help" in arguments:
+        # print help message if asked
+        print("The program outputs a multiplication table up to the input number. Usage: ")
+        print("python3 %s N "%arguments[0])
+        print("where N is a positive integer")
+        sys.exit(0)
+    else:
+        if len(arguments)<2:
+            # check if number provided
+            print("Wrong input, see help:")
+            print("python3 %s help "%arguments[0])
+            sys.exit(0)
+
+def readin_integer(arguments):
+    check_command_line(arguments)
+    N_input = arguments[1]
+    #making sure N is integer
+    try:
+        N = int(N_input)
+        if N > 1:
+            return N
+        else:
+            print("N should be > 0")
+            sys.exit(0)
+    except ValueError:
+        print("Input number N = %s is not an integer"%N_input)
+        sys.exit(0)
+def generate_mult_table(N):
+    table = np.zeros((N, N), dtype=int)
+    for i in range(N):
+        for j in range(N):
+            # assigning multiplication table
+            table[i,j] = (i+1)*(j+1)
+    return(table)
--- mult_table.py
+++ mult_table.py
@@ -8,37 +8,13 @@ import sys,os
 sys.path.append(os.getcwd())
 import module
 from module import *
-import numpy as np
 
 #Reading command line arguments
-if "help" in sys.argv:
-    # print help message if asked
-    print("The program outputs a multiplication table up to the input number. Usage: ")
-    print("python3 %s N "%sys.argv[0])
-    print("where N is an integer number")
-    sys.exit(0)
-else:
-    if len(sys.argv)<2:
-        # check if number provided
-        print("Wrong input, see help:")
-        print("python3 %s help "%sys.argv[0])
-        sys.exit(0)
-    else:
-        N_input = sys.argv[1]
-        #making sure N is integer
-        if if_integer(N_input):
-            N = int(N_input)
-            print("Multiplication table for N = %i"%N)
-        else:
-            print("Input number N = %s is not an integer"%N_input)
-            sys.exit(0)
-
+N = readin_integer(sys.argv)
+print("Multiplication table for N = %i"%N)
 # main program starts here
-table = np.zeros((N, N), dtype=int)
-for i in range(N):
-    for j in range(N):
-        # assigning multiplication table
-        table[i,j] = (i+1)*(j+1)
+table = generate_mult_table(N)
+
 
 #printing the table with pandas module
 import pandas
